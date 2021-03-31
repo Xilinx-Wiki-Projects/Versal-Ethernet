f501b4ed7fdf57ad9001d77ac396c02c0883cfde
Praveen Venugopal <pvenugo@xlnx.xilinx.com>
2021-03-31 18:17:23 +0530

    Merge branch 'master' of https://github.com/pvenugo786/VCK190-Ethernet
    Added Versal 1G design

---
+++ pl_eth_1G/scripts/axi_eth_subsys_top.tcl
@@ -0,0 +1,68 @@
+# Copyright 2021 Xilinx Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Set the name of the project:
+set project_name axi_eth_subsys
+
+# Set the project device:
+set device xcvc1902-vsva2197-2MP-e-S-es1
+
+
+#Set the path to the constraints file:
+set impl_const ../hardware/vck190_${project_name}.xdc
+
+# If using a UI layout, uncomment this line:
+#set ui_name layout.ui
+
+
+# Set the path to the directory we want to put the Vivado build in. Convention is <PROJECT NAME>_hw
+set proj_dir ../pl_axi1G_vck190/${project_name}_hw
+
+
+create_project -name ${project_name} -force -dir ${proj_dir} -part ${device}
+
+set_property board_part xilinx.com:vck190_es:part0:1.0 [current_project]
+
+# Source the BD file, BD naming convention is <PROJECT_NAME>_bd.tcl
+source ${project_name}_bd.tcl
+
+add_files -fileset constrs_1 -norecurse ./${impl_const}
+set_property used_in_synthesis true [get_files ./${impl_const}]
+
+make_wrapper -files [get_files ${proj_dir}/${project_name}.srcs/sources_1/bd/${project_name}/${project_name}.bd] -top
+
+add_files -norecurse ${proj_dir}/${project_name}.srcs/sources_1/bd/${project_name}/hdl/${project_name}_wrapper.v
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+
+validate_bd_design
+save_bd_design
+close_bd_design ${project_name}
+
+# If using UI, uncomment these two lines:
+#file mkdir ${proj_dir}/${project_name}.srcs/sources_1/bd/${project_name}/ui
+#file copy -force ${ui_name} ${proj_dir}/${project_name}.srcs/sources_1/bd/${project_name}/ui/${ui_name}
+
+open_bd_design ${proj_dir}/${project_name}.srcs/sources_1/bd/${project_name}/${project_name}.bd
+set_property synth_checkpoint_mode None [get_files ${proj_dir}/${project_name}.srcs/sources_1/bd/${project_name}/${project_name}.bd]
+
+launch_runs synth_1 -jobs 4
+wait_on_run synth_1
+launch_runs impl_1 -jobs 4
+wait_on_run impl_1
+open_run impl_1
+launch_runs impl_1 -to_step write_device_image -jobs 4
+wait_on_run impl_1
+file mkdir ${proj_dir}/${project_name}/XSA
+write_hw_platform -fixed -include_bit -force -file ${proj_dir}/${project_name}/XSA/${project_name}_wrapper.xsa
